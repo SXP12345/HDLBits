40d1d05378183b060198622de983102ba53386c2
SXP12345 <72686850+SXP12345@users.noreply.github.com>
2023-08-10 09:38:29 +0800

    Create top_module.v

---
+++ Verilog_Language/Procedures/Always_case2/top_module.v
@@ -0,0 +1,17 @@
+// synthesis verilog_input_version verilog_2001
+
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+    always@(*)
+        begin
+        casex(in)
+            4'b???1: pos = 2'b00;
+            4'b??10: pos = 2'b01;
+            4'b?100: pos = 2'b10;
+            4'b1000: pos = 2'b11;
+            default: pos = 0;
+        endcase
+        end
+ 
+endmodule
